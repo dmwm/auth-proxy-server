c343c91215ab9eb0a03b0c5d470f73e9b815a948
Valentin Kuznetsov <vkuznet@gmail.com>
2020-07-10 13:36:53 -0400

    Add support for x509 authentication: read RootCAs, verify user info with cric; perform x509 validation and verification

--- proxy_auth_server.go
+++ proxy_auth_server.go
@@ -39,6 +39,8 @@ Reverse proxy examples:
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -72,7 +74,8 @@ type Ingress struct {
 // Configuration stores server configuration parameters
 type Configuration struct {
 	Port                int       `json:"port"`                   // server port number
-	X509Port            int       `json:"x509port"`               // server port number for x509 clients
+	X509                bool      `json:"x509"`                   // run x509 auth server
+	RootCAs             []string  `json:"rootCAs"`                // server Root CA
 	Base                string    `json:"base"`                   // base URL
 	ClientID            string    `json:"client_id"`              // OICD client id
 	ClientSecret        string    `json:"client_secret"`          // OICD client secret
@@ -727,28 +730,69 @@ func auth_proxy_server(serverCrt, serverKey string) {
 	http.HandleFunc(fmt.Sprintf("%s/callback", Config.Base), serverCallbackHandler)
 
 	// the request handler
-	//     serverMux := http.NewServeMux()
-	//     serverMux.HandleFunc("/", serverRequestHandler)
 	http.HandleFunc("/", serverRequestHandler)
 
 	// start HTTP or HTTPs server based on provided configuration
 	addr := fmt.Sprintf(":%d", Config.Port)
 	if serverCrt != "" && serverKey != "" {
-		//start HTTPS server which require user certificates
-		//         server := &http.Server{Addr: addr, Handler: serverMux}
 		server := &http.Server{Addr: addr}
 		log.Printf("Starting HTTPs server on %s", addr)
 		log.Fatal(server.ListenAndServeTLS(serverCrt, serverKey))
 	} else {
 		// Start server without user certificates
 		log.Printf("Starting HTTP server on %s", addr)
-		//         log.Fatal(http.ListenAndServe(addr, serverMux))
 		log.Fatal(http.ListenAndServe(addr, nil))
 	}
 }
 
+// helper function to find user info in cric records for given cert subject
+func findUser(subjects []string) (cmsauth.CricEntry, error) {
+	for _, r := range CricRecords {
+		for _, s := range subjects {
+			if strings.HasSuffix(r.DN, s) {
+				return r, nil
+			}
+		}
+	}
+	msg := fmt.Sprintf("user not found: %v\n", subjects)
+	return cmsauth.CricEntry{}, errors.New(msg)
+}
+
 // x509RequestHandler handle requests for x509 clients
 func x509RequestHandler(w http.ResponseWriter, r *http.Request) {
+	// get client CAs
+	if r.TLS != nil {
+		certs := r.TLS.PeerCertificates
+		userData := make(map[string]interface{})
+		for _, asn1Data := range certs {
+			cert, err := x509.ParseCertificate(asn1Data.Raw)
+			if err != nil {
+				log.Println("x509RequestHandler tls: failed to parse certificate from server: " + err.Error())
+			}
+			start := time.Now()
+			rec, err := findUser(strings.Split(cert.Subject.String(), ","))
+			if Config.Verbose > 0 {
+				log.Println("find user", rec, err, time.Since(start))
+			}
+			if err == nil {
+				userData["issuer"] = cert.Issuer.String()
+				userData["Subject"] = cert.Subject.String()
+				userData["name"] = rec.Name
+				userData["cern_upn"] = rec.Login
+				userData["cern_person_id"] = rec.ID
+				userData["auth_time"] = time.Now().Unix()
+				userData["exp"] = cert.NotAfter.Unix()
+				userData["email"] = cert.EmailAddresses
+			} else {
+				log.Println("unauthorized access", err)
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+		}
+		// set CMS headers based on provided user certificate
+		CMSAuth.SetCMSHeaders(r, userData, CricRecords, true)
+	}
+	// check CMS headers
 	status := CMSAuth.CheckAuthnAuthz(r.Header)
 	if Config.Verbose > 0 {
 		log.Println("x509RequestHandler", r.Header, status)
@@ -764,21 +808,77 @@ func x509RequestHandler(w http.ResponseWriter, r *http.Request) {
 func x509_proxy_server(serverCrt, serverKey string) {
 
 	// the request handler
-	serverMux := http.NewServeMux()
-	serverMux.HandleFunc("/", x509RequestHandler)
-	//     http.HandleFunc("/", x509RequestHandler)
+	http.HandleFunc("/", x509RequestHandler)
 
 	// start HTTP or HTTPs server based on provided configuration
-	addr := fmt.Sprintf(":%d", Config.X509Port)
+	rootCAs := x509.NewCertPool()
+	for _, fname := range Config.RootCAs {
+		caCert, err := ioutil.ReadFile(fname)
+		if err != nil {
+			log.Fatal("Unable to read RootCA, %s\n", fname)
+		}
+		log.Println("Load", fname)
+		//         rootCAs.AppendCertsFromPEM(caCert)
+		if ok := rootCAs.AppendCertsFromPEM(caCert); !ok {
+			log.Fatal("invalid PEM format while importing trust-chain: %q", fname)
+		}
+	}
+
+	tlsConfig := &tls.Config{
+		// Set InsecureSkipVerify to skip the default validation we are
+		// replacing. This will not disable VerifyPeerCertificate.
+		InsecureSkipVerify: true,
+		ClientAuth:         tls.RequestClientCert,
+		RootCAs:            rootCAs,
+	}
+	// see concrete example here:
+	// https://golang.org/pkg/crypto/tls/#example_Config_verifyPeerCertificate
+	// https://www.example-code.com/golang/cert.asp
+	// https://golang.org/pkg/crypto/x509/pkix/#Extension
+	tlsConfig.VerifyPeerCertificate = func(certificates [][]byte, _ [][]*x509.Certificate) error {
+		if Config.Verbose > 1 {
+			log.Println("call custom tlsConfig.VerifyPeerCertificate")
+		}
+		certs := make([]*x509.Certificate, len(certificates))
+		for i, asn1Data := range certificates {
+			cert, err := x509.ParseCertificate(asn1Data)
+			if err != nil {
+				return errors.New("tls: failed to parse certificate from server: " + err.Error())
+			}
+			if Config.Verbose > 1 {
+				log.Println("Issuer", cert.Issuer)
+				log.Println("Subject", cert.Subject)
+				log.Println("emails", cert.EmailAddresses)
+			}
+			// check validity of user certificate
+			tstamp := time.Now().Unix()
+			if cert.NotBefore.Unix() > tstamp || cert.NotAfter.Unix() < tstamp {
+				msg := fmt.Sprintf("Expired user certificate, valid from %v to %v\n", cert.NotBefore, cert.NotAfter)
+				return errors.New(msg)
+			}
+			certs[i] = cert
+		}
+		opts := x509.VerifyOptions{
+			Roots:         rootCAs,
+			Intermediates: x509.NewCertPool(),
+			KeyUsages:     []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		}
+		for _, cert := range certs[1:] {
+			opts.Intermediates.AddCert(cert)
+		}
+		_, err := certs[0].Verify(opts)
+		return err
+	}
+	addr := fmt.Sprintf(":%d", Config.Port)
 	if serverCrt != "" && serverKey != "" {
 		//start HTTPS server which require user certificates
-		server := &http.Server{Addr: addr, Handler: serverMux}
+		server := &http.Server{Addr: addr, TLSConfig: tlsConfig}
 		log.Printf("Starting x509 HTTPs server on %s", addr)
 		log.Fatal(server.ListenAndServeTLS(serverCrt, serverKey))
 	} else {
 		// Start server without user certificates
 		log.Printf("Starting x509 HTTP server on %s", addr)
-		log.Fatal(http.ListenAndServe(addr, serverMux))
+		log.Fatal(http.ListenAndServe(addr, nil))
 	}
 }
 
@@ -802,6 +902,19 @@ func main() {
 			verbose := false
 			if Config.Verbose > 2 {
 				verbose = true
+			}
+			// if cric file is given read it first, then if we have
+			// cric url we'll update it from there
+			if Config.CricFile != "" {
+				cricRecords, err = cmsauth.ParseCric(Config.CricFile, verbose)
+				log.Printf("obtain CRIC records from %s, %v", Config.CricFile, err)
+				if err != nil {
+					log.Printf("Unable to update CRIC records: %v", err)
+				} else {
+					CricRecords = cricRecords
+					keys := reflect.ValueOf(CricRecords).MapKeys()
+					log.Println("Updated CRIC records", len(keys))
+				}
 			}
 			var err error
 			for {
@@ -832,16 +945,15 @@ func main() {
 		}()
 		_, e1 := os.Stat(Config.ServerCrt)
 		_, e2 := os.Stat(Config.ServerKey)
+		var crt, key string
 		if e1 == nil && e2 == nil {
-			if Config.X509Port > 0 {
-				go x509_proxy_server(Config.ServerCrt, Config.ServerKey)
-			}
-			auth_proxy_server(Config.ServerCrt, Config.ServerKey)
-		} else {
-			if Config.X509Port > 0 {
-				go x509_proxy_server("", "")
-			}
-			auth_proxy_server("", "")
+			crt = Config.ServerCrt
+			key = Config.ServerKey
 		}
+		if Config.X509 {
+			x509_proxy_server(crt, key)
+			return
+		}
+		auth_proxy_server(crt, key)
 	}
 }
