74a8a38c09e75ad3794af1b3b654883d90338b58
Valentin Kuznetsov <vkuznet@gmail.com>
2020-10-15 09:06:56 -0400

    Add new test

--- x509_test.go
+++ x509_test.go
@@ -1,54 +1,25 @@
 package main
 
 import (
-	"log"
+	"net/http"
+	"net/http/httptest"
 	"testing"
-
-	"github.com/dmwm/cmsauth"
-	"github.com/stretchr/testify/assert"
 )
 
-// Test findCN function
-func Test_findCN(t *testing.T) {
-	subject := "CN=First Last OU=Organic Units+OU=Users"
-	cn, err := findCN(subject)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, cn, "CN=First Last")
-	subject = "O=IT CN=First Last OU=Organic Units+OU=Users"
-	cn, err = findCN(subject)
-	assert.Equal(t, err, nil)
-	assert.Equal(t, cn, "CN=First Last")
-}
+// TestX509RequestHandler provides test of GET method for our service
+func TestX509RequestHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-// Test fundUser function
-func Test_findUser(t *testing.T) {
-	CricRecords = make(cmsauth.CricRecords)
-	var dns []string
-	dn1 := "/DC=org/DC=dc/DC=tcs/C=country/O=Institute Test/CN=First Last"
-	dn2 := "/DC=org/DC=dc/DC=tcs/C=country/O=Institute Test/CN=First Last name@email.com"
-	dns = append(dns, dn1)
-	dns = append(dns, dn2)
-	rec := cmsauth.CricEntry{Login: "name", DNs: dns}
-	CricRecords["name"] = rec
-	var subjects []string
-	s := "CN=First Last OU=Organic Units+OU=Users"
-	subjects = append(subjects, s)
-	_, err := findUser(subjects)
-	assert.Equal(t, err, nil)
-}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(x509RequestHandler)
+	handler.ServeHTTP(rr, req)
 
-// Benchmark findUser function
-func Benchmark_findUser(b *testing.B) {
-	CricRecords = make(cmsauth.CricRecords)
-	rec := cmsauth.CricEntry{DN: "/DC=org/DC=dc/DC=tcs/C=country/O=Institute Test/CN=First Last"}
-	CricRecords["login"] = rec
-	var subjects []string
-	s := "CN=First Last OU=Organic Units+OU=Users"
-	subjects = append(subjects, s)
-	for n := 0; n < b.N; n++ {
-		_, err := findUser(subjects)
-		if err != nil {
-			log.Fatal(err)
-		}
+	// since we don't provide valid cert we should get http.StatusUnauthorized
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
 	}
 }
