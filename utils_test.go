ff239dbf12b74f917826cd56399f3f1c021cf25f
Valentin Kuznetsov <vkuznet@gmail.com>
2020-10-16 13:45:31 -0400

    Add benchmark tests

--- utils_test.go
+++ utils_test.go
@@ -50,8 +50,13 @@ func Test_findUser(t *testing.T) {
 func Benchmark_findUser(b *testing.B) {
 	CricRecords = make(cmsauth.CricRecords)
 	cmsRecords = make(cmsauth.CricRecords)
-	rec := cmsauth.CricEntry{DN: "/DC=org/DC=dc/DC=tcs/C=country/O=Institute Test/CN=First Last"}
-	CricRecords["login"] = rec
+	var dns []string
+	dn1 := "/DC=org/DC=dc/DC=tcs/C=country/O=Institute Test/CN=First Last"
+	dn2 := "/DC=org/DC=dc/DC=tcs/C=country/O=Institute Test/CN=First Last name@email.com"
+	dns = append(dns, dn1)
+	dns = append(dns, dn2)
+	rec := cmsauth.CricEntry{Login: "name", DNs: dns}
+	CricRecords["name"] = rec
 	updateCMSRecords(CricRecords)
 	var subjects []string
 	s := "CN=First Last"
--- x509_test.go
+++ x509_test.go
@@ -23,3 +23,24 @@ func TestX509RequestHandler(t *testing.T) {
 			status, http.StatusOK)
 	}
 }
+
+// BenchmarkX509RequestHandler provides test of GET method for our service
+func BenchmarkX509RequestHandler(b *testing.B) {
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(x509RequestHandler)
+
+	// perform benchmark test
+	for n := 0; n < b.N; n++ {
+		handler.ServeHTTP(rr, req)
+		// since we don't provide valid cert we should get http.StatusUnauthorized
+		if status := rr.Code; status != http.StatusUnauthorized {
+			b.Errorf("handler returned wrong status code: got %v want %v",
+				status, http.StatusOK)
+		}
+	}
+}
