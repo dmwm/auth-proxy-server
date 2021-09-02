ce6d89ea9dcaac48b4e8981b618f6fc8d0cb44dc
Valentin Kuznetsov <vkuznet@gmail.com>
2021-09-02 13:23:33 -0400

    update

--- utils_test.go
+++ utils_test.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"log"
 	"testing"
 
-	"github.com/dmwm/cmsauth"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -25,48 +23,6 @@ func Test_findCN(t *testing.T) {
 	cn, err = findCN(subject)
 	assert.Equal(t, err, nil)
 	assert.Equal(t, cn, "CN=First Last")
-}
-
-// Test fundUser function
-func Test_findUser(t *testing.T) {
-	CricRecords = make(cmsauth.CricRecords)
-	cmsRecords = make(cmsauth.CricRecords)
-	var dns []string
-	dn1 := "/DC=org/DC=dc/DC=tcs/C=country/O=Institute Test/CN=First Last"
-	dn2 := "/DC=org/DC=dc/DC=tcs/C=country/O=Institute Test/CN=First Last name@email.com"
-	dns = append(dns, dn1)
-	dns = append(dns, dn2)
-	rec := cmsauth.CricEntry{Login: "name", DNs: dns}
-	CricRecords["name"] = rec
-	updateCMSRecords(CricRecords)
-	var subjects []string
-	s := "CN=First Last"
-	subjects = append(subjects, s)
-	_, err := findUser(subjects)
-	assert.Equal(t, err, nil)
-}
-
-// Benchmark findUser function
-func Benchmark_findUser(b *testing.B) {
-	CricRecords = make(cmsauth.CricRecords)
-	cmsRecords = make(cmsauth.CricRecords)
-	var dns []string
-	dn1 := "/DC=org/DC=dc/DC=tcs/C=country/O=Institute Test/CN=First Last"
-	dn2 := "/DC=org/DC=dc/DC=tcs/C=country/O=Institute Test/CN=First Last name@email.com"
-	dns = append(dns, dn1)
-	dns = append(dns, dn2)
-	rec := cmsauth.CricEntry{Login: "name", DNs: dns}
-	CricRecords["name"] = rec
-	updateCMSRecords(CricRecords)
-	var subjects []string
-	s := "CN=First Last"
-	subjects = append(subjects, s)
-	for n := 0; n < b.N; n++ {
-		_, err := findUser(subjects)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
 }
 
 // Test_InList function
